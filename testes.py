4c6a7fa2ed6522e9e04a7c0ea4d51e28d7692bd8
Ricardo <ricardobassanesi@gmail.com>
2025-07-12 14:58:20 -0300

    ajuste teste

--- testes.py
+++ testes.py
@@ -1,7 +1,9 @@
 import requests
 
+token = input("Digite o token: ")
+
 headers = {
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI4IiwiZXhwIjoxNzUyMzQzNDkyfQ.TRaVw5OEhB5fSomKzHfzA12AQld40rfSFLVWtTQC3PA"
+    "Authorization": f"Bearer {token}"
 }
 
 requisicao = requests.get("http://127.0.0.1:8000/auth/refresh", headers=headers)
