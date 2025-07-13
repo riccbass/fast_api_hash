f915daeb7d7a3c2195fabd1263588d5ba6cd068e
Ricardo <ricardobassanesi@gmail.com>
2025-07-12 22:09:39 -0300

    aula 8

--- order_routes.py
+++ order_routes.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, Depends
-from dependencies import pegar_sessao
+from fastapi import APIRouter, Depends, HTTPException
+from dependencies import pegar_sessao, verificar_token
 from sqlalchemy.orm import Session
 from schemas import PedidoSchema
-from models import Pedido
+from models import Pedido, Usuario
 
-order_router = APIRouter(prefix="/pedidos", tags=["pedidos"])
+order_router = APIRouter(prefix="/pedidos", tags=["pedidos"], dependencies=[Depends(verificar_token)])
 
 @order_router.get("/")
 async def pedidos():
@@ -21,3 +21,29 @@ async def criar_pedido(pedido_schema: PedidoSchema, session: Session = Depends(p
     session.commit()
 
     return {"mensagem":f"Pedido criado com sucesso. ID do pedido {novo_pedido.id}"}
+
+@order_router.post("/pedido/cancelar/{id_pedido}")
+async def cancelar_pedido(id_pedido: int, 
+                          session: Session = Depends(pegar_sessao),
+                          usuario: Usuario = Depends(verificar_token)):
+    
+    #usuario.admin = True
+    #usuario.id = pedido.usuario
+
+    pedido = session.query(Pedido).filter(Pedido.id == id_pedido).first()
+
+    if not pedido:
+        raise HTTPException(status_code=400, detail="Pedido não encontrado")
+    
+    if not usuario.admin or usuario.id != pedido.usuario: # type: ignore
+        raise HTTPException(status_code=401, detail="Voce não tem autorização para fazer essa modificação")        
+
+    pedido.status = "CANCELADO"
+    session.commit()
+    return {
+        "mensagem": f"Pedido número: {pedido.id} cancelado com sucesso", #passando o campo, 
+        #obriga o modelo a importar todos os campos
+        "pedido": pedido
+    }
+
+
